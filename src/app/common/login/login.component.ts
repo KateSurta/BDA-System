bbd47a5c2342ab08bbcbc7da6da8c1fb574a050d
KateSurta <ekaterina.surta1992@gmail.com>
2017-12-05 00:03:53 +0300

    create report page

--- src/app/common/login/login.component.ts
+++ src/app/common/login/login.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public email: string;
+  public password: string;
 
   ngOnInit() {
   }
